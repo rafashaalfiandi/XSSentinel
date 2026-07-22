d59e8bc841956571bd112fa0b8692ad30566d1c5
Rafasha Alfiandi Passage <rafashacode.id@gmail.com>
2026-07-23 00:55:28 +0700

    Delete xssentinel_core directory

--- xssentinel_core/runtime/engine/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Engine helpers for XSSentinel."""
-
-from .rafashacodeid import AUTHOR_URL, BANNER, FINGERPRINT_ID, rafashacodeid, verify_identity
-
-__all__ = ["AUTHOR_URL", "BANNER", "FINGERPRINT_ID", "rafashacodeid", "verify_identity"]
